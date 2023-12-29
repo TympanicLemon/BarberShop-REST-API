3a996190992b894d7a69873c0488f978d25c2f8e
Daniel Zapata <zdaniel9799@gmail.com>
2023-12-28 20:58:10 -0800

    Refactor CustomerService to use DataFactory for data conversion

--- src/main/java/barber/shop/service/CustomerService.java
+++ src/main/java/barber/shop/service/CustomerService.java
@@ -16,82 +16,82 @@ import java.util.Optional;
 
 @Service
 public class CustomerService {
+    @Autowired
+    private CustomerDao customerDao;
 
-  @Autowired
-  private CustomerDao customerDao;
+    @Autowired
+    private BarberShopDao barberShopDao;
 
-  @Autowired
-  private BarberShopDao barberShopDao;
+    @Autowired
+    private DataFactory dataFactory;
 
-  @Transactional
-  public CustomerData createCustomer(Long barberShopId, CustomerData customerData) {
-    Optional<Customer> existingCustomer = customerDao.findByEmail(customerData.getEmail());
-    if(existingCustomer.isPresent()) {
-      throw new DuplicateKeyException("Customer with email " + customerData.getEmail() + " already exists");
+    @Transactional
+    public CustomerData createCustomer(Long barberShopId, CustomerData customerData) {
+        customerDao.findByEmail(customerData.getEmail()).ifPresent(c -> {
+                    throw new DuplicateKeyException("Customer with email " + customerData.getEmail() + " already exists.");
+        });
+
+        BarberShop barberShop = findBarberShopById(barberShopId);
+        Customer customer = dataFactory.convertToCustomer(customerData);
+        barberShop.getCustomers().add(customer);
+
+        return dataFactory.convertToCustomerData(customerDao.save(customer));
     }
 
-    BarberShop barberShop = findBarberShopById(barberShopId);
-    Customer newCustomer = customerData.toCustomer();
+    @Transactional
+    public CustomerData updateCustomer(Long customerId, CustomerData customerData) {
+        Customer customer = findCustomerById(customerId);
 
-    barberShop.getCustomers().add(newCustomer);
+        if (!customer.getEmail().equals(customerData.getEmail())) {
+            Optional<Customer> optCustomer = customerDao.findByEmail(customerData.getEmail());
+            if (optCustomer.isPresent()) {
+                throw new DuplicateKeyException("Customer with email " + customerData.getEmail() + " already exists");
+            }
+        }
 
-    return new CustomerData(customerDao.save(newCustomer));
-  }
+        updateFieldsForCustomer(customer, customerData);
+        dataFactory.convertToCustomer(customerData);
 
-  @Transactional
-  public CustomerData updateCustomer(Long customerId, CustomerData customerData) {
-    Customer customer = findCustomerById(customerId);
-
-    if(!customer.getEmail().equals(customerData.getEmail())) {
-      Optional<Customer> optCust = customerDao.findByEmail(customerData.getEmail());
-      if(optCust.isPresent()) {
-        throw new DuplicateKeyException("Customer with email " + customerData.getEmail() + " already exists");
-      }
+        return dataFactory.convertToCustomerData(customerDao.save(customer));
     }
 
-    setFieldsInCustomer(customer, customerData);
-
-    return new CustomerData(customerDao.save(customer));
-  }
-
-  private void setFieldsInCustomer(Customer customer, CustomerData customerData) {
-    customer.setFirstName(customerData.getFirstName());
-    customer.setLastName(customerData.getLastName());
-    customer.setEmail(customerData.getEmail());
-  }
-
-  private Customer findCustomerById(Long customerId) {
-    return customerDao.findById(customerId).orElseThrow(() -> new NoSuchElementException(
-      "Customer with ID=" + customerId + " was not found"));
-  }
-
-  private BarberShop findBarberShopById(Long barberShopId) {
-    return barberShopDao.findById(barberShopId).orElseThrow(() -> new NoSuchElementException(
-      "Barbershop with ID=" + barberShopId + " was not found"));
-  }
-
-  @Transactional(readOnly = true)
-  public CustomerData getCustomer(Long customerId) {
-    Customer customer = findCustomerById(customerId);
-    return new CustomerData(customer);
-  }
-
-  @Transactional
-  public void deleteCustomer(Long customerId) {
-    Customer customer = findCustomerById(customerId);
-
-    // Clear associations on both sides
-    for (BarberShop barberShop : customer.getBarberShops()) {
-      barberShop.getCustomers().remove(customer);
+    private void updateFieldsForCustomer(Customer customer, CustomerData customerData) {
+        customer.setFirstName(customerData.getFirstName());
+        customer.setLastName(customerData.getLastName());
+        customer.setEmail(customerData.getEmail());
     }
-    customer.getBarberShops().clear();
 
-    customerDao.delete(customer);
-  }
+    private Customer findCustomerById(Long customerId) {
+        return customerDao.findById(customerId).orElseThrow(() -> new NoSuchElementException(
+                "Customer with ID=" + customerId + " was not found"));
+    }
 
-  public List<CustomerData> getAllCustomers() {
-    return customerDao.findAll().stream().map(CustomerData::new).toList();
-  }
+    private BarberShop findBarberShopById(Long barberShopId) {
+        return barberShopDao.findById(barberShopId).orElseThrow(() -> new NoSuchElementException(
+                "Barbershop with ID=" + barberShopId + " was not found"));
+    }
+
+    @Transactional(readOnly = true)
+    public CustomerData getCustomer(Long customerId) {
+        Customer customer = findCustomerById(customerId);
+        return dataFactory.convertToCustomerData(customer);
+    }
+
+    @Transactional
+    public void deleteCustomer(Long customerId) {
+        Customer customer = findCustomerById(customerId);
+
+        for (BarberShop barberShop : customer.getBarberShops()) {
+            barberShop.getCustomers().remove(customer);
+        }
+        customer.getBarberShops().clear();
+
+        customerDao.delete(customer);
+    }
+
+    public List<CustomerData> getAllCustomers() {
+        return customerDao.findAll().stream().map(c -> dataFactory.convertToCustomerData(c)).toList();
+    }
 }
 
 
